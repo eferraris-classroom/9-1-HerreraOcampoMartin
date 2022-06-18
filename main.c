20285066c673722fb7ad01f3fabfd983c3556d79
Martin <marthofn@gmail.com>
2022-06-18 11:38:50 -0300

    Archivos que faltan

---
+++ main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "Inmobiliaria.h"
+
+int main() {
+
+    ListaPropiedades *lista = crearListaPropiedades();
+
+    agregarPropiedad(lista, crearPropiedad("Saavedra", "Direccion en Saavedra 1", 129000, 2));
+    agregarPropiedad(lista, crearPropiedad("Belgrano", "Direccion en Belgrano 1", 254000, 3));
+    agregarPropiedad(lista, crearPropiedad("Belgrano", "Direccion en Belgrano 2", 654000, 4));
+    agregarPropiedad(lista, crearPropiedad("Saavedra", "Direccion en Saavedra 2", 29000, 1));
+    agregarPropiedad(lista, crearPropiedad("Palermo", "Direccion en Palermo 1", 2295000, 11));
+    agregarPropiedad(lista, crearPropiedad("Colegiales", "Direccion en Colegiales 1", 867000, 5));
+    agregarPropiedad(lista, crearPropiedad("Saavedra", "Direccion en Saavedra 3", 678000, 4));
+    agregarPropiedad(lista, crearPropiedad("Colegiales", "Direccion en Colegiales 2", 1889000, 6));
+    agregarPropiedad(lista, crearPropiedad("Palermo", "Direccion en Palermo 2", 6478000, 15));
+
+    mostrarPropiedades(lista);
+
+    // --------------------------------------------------
+
+    char *barrio;
+    float precio;
+    int ambientes;
+
+    printf("INGRESE EL BARRIO DE INTERÉS: ");
+    fgets(barrio, 100, stdin);
+    fflush(stdin);
+
+    printf("INGRESE EL PRECIO MÁXIMO: ");
+    scanf("%f", &precio);
+    fflush(stdin);
+
+    printf("INGRESE LA CANTIDAD DE AMBIENTES MÍNIMOS: ");
+    scanf("%d", &ambientes);
+    fflush(stdin);
+
+    filtrar(lista, barrio, precio, ambientes);
+
+    return 0;
+}
