d845bd15f8f5f376710a64a5bf04989b31c41e1e
Martin <marthofn@gmail.com>
2022-06-18 11:12:31 -0300

    Agregar CrearListaPropiedades y CrearPropiedades

--- Inmobiliaria.c
+++ Inmobiliaria.c
@@ -1,1 +1,34 @@
 #include "Inmobiliaria.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+ListaPropiedades *crearListaPropiedades() {
+    ListaPropiedades *lista = (ListaPropiedades *) malloc(sizeof (ListaPropiedades));
+
+    if(lista == NULL){
+        printf("No hay memoria libre.");
+        exit(-1);
+    }
+
+    lista->tam = 0;
+    lista->primera = NULL;
+
+    return lista;
+}
+
+Propiedad *crearPropiedad(char *barrio, char *direccion, float precio, int ambientes) {
+    Propiedad *nueva = (Propiedad *) malloc(sizeof (Propiedad));
+
+    if(nueva == NULL){
+        printf("No hay memoria libre.");
+        exit(-1);
+    }
+
+    nueva->siguiente = NULL;
+    nueva->precio = precio;
+    nueva->barrio = barrio;
+    nueva->direccion = direccion;
+    nueva->ambientes = ambientes;
+
+    return nueva;
+}
--- Inmobiliaria.h
+++ Inmobiliaria.h
@@ -1,6 +1,23 @@
 #ifndef INC_9_1_HERRERAOCAMPOMARTIN_INMOBILIARIA_H
 #define INC_9_1_HERRERAOCAMPOMARTIN_INMOBILIARIA_H
 
+typedef struct propiedad {
+    char *barrio;
+    char *direccion;
+    float precio;
+    int ambientes;
+    struct propiedad *siguiente;
+} Propiedad;
 
+typedef struct listaPropiedades {
+    int tam;
+    Propiedad *primera;
+} ListaPropiedades;
+
+ListaPropiedades *crearListaPropiedades();
+Propiedad *crearPropiedad(char *barrio, char *direccion, float precio, int ambientes);
+void agregarPropiedad(ListaPropiedades *lista, Propiedad *nueva);
+void filtrar(char *barrio, float precioMaximo, int ambientesMinimos);
+void mostrarPropiedades(ListaPropiedades *lista);
 
 #endif
